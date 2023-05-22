3601376882ed8d3a99a3147860269d27d47f5fb8
Hareen Bejjanki <bejjanki.hareen@gmail.com>
2023-05-22 21:06:20 +0530

    initial commit to git

---
+++ src/main/java/com/anvizent/elt/commons/constants/Constants.java
@@ -0,0 +1,32 @@
+package com.anvizent.elt.commons.constants;
+
+/**
+ * @author Hareen Bejjanki
+ * @author Apurva Deshmukh
+ *
+ */
+public class Constants {
+
+	public static class ELTConstants {
+
+		public static final String ERROR_DATA = "error_data_";
+		public static final String EXTERNAL_DATA = "external_data_";
+		public static final String ERROR_TYPE = "error_type";
+		public static final String ERROR_MESSAGE = "error_message";
+		public static final String ERROR_FULL_DETAILS = "error_full_details";
+		public static final String JOB_NAME = "job_name";
+		public static final String COMPONENT = "component";
+		public static final String COMPONENT_NAME = "component_name";
+		public static final String INTERNAL_RDD_NAME = "internal_rdd_name";
+		public static final String SPECIAL_NAME = "special_name";
+		public static final String STATS_NAME = "stats_name";
+		public static final String STATS_VALUE = "stats_value";
+		public static final String COMPONENT_LEVEL = "component_level";
+		public static final String CREATED_BY_DEFAULT = "anvizent_elt_system";
+		public static final String CREATED_BY = "created_by";
+		public static final String CREATED_DATE = "created_date";
+		public static final String UPDATED_BY = "updated_by";
+		public static final String UPDATED_DATE = "updated_date";
+	}
+
+}
